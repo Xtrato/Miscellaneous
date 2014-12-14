b6700490cbe3a832d598e8eb02f69bbbde1c2dbf
James <james.woolley5@googlemail.com>
2014-12-14 14:07:27 +0000

    Added VNC related scripts

--- Security/masscanDuplicateRemoval.py
+++ Security/masscanDuplicateRemoval.py
@@ -1,4 +1,4 @@
-#This is a script used in conjunction with the VNC Authentication checker to remove duplicates from a masscan log file.
+#This is a script used in conjunction with the NTP monlist checker to remove duplicates from a masscan log file.
 #It parses through the masscan output called ntp.xml and produces a file called port123.txt.
 #More information can be found http://jamesdotcom.com/?p=578
 from lxml import etree
